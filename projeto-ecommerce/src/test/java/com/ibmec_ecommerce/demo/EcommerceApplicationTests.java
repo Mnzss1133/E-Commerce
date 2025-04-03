d84cc36ac6be3feb6a7cc038fb752465d830dd78
Joao Pedro Passos Ribeiro de Menezes <86020181+Mnzss1133@users.noreply.github.com>
2025-04-03 20:52:19 -0300

    Add files via upload

---
+++ projeto-ecommerce/src/test/java/com/ibmec_ecommerce/demo/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmec_ecommerce.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
