cbd546b8e58f48ba9ba07838cb7907fe7a336a01
Joao Pedro Passos Ribeiro de Menezes <joaopedromenezes890@gmail.com>
2025-04-08 12:39:19 -0300

    excluindo o target

--- src/main/java/ibmec_ecommerce/ecommerce/Controllers/ProdutoController.java
+++ src/main/java/ibmec_ecommerce/ecommerce/Controllers/ProdutoController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import ibmec_ecommerce.ecommerce.Repository.Cosmos.ProdutoRepositorio;
 import java.util.List;
 import java.util.Optional;
 
