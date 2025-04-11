b882a0e672ef7c891316e3fe0e3224cefdbb55b5
Joao Pedro Passos Ribeiro de Menezes <joaopedromenezes890@gmail.com>
2025-04-10 22:38:52 -0300

    dtNascimento

--- src/main/java/ibmec_ecommerce/ecommerce/Model/Usuario.java
+++ src/main/java/ibmec_ecommerce/ecommerce/Model/Usuario.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 
 
@@ -30,6 +31,7 @@ public class Usuario {
     @Column
     private String email;
 
+    @JsonFormat(pattern = "dd/MM/yyyy")
     @Column
     private LocalDateTime dtNascimento;
 
